d07a0bce5da2a55c8844f3e7a9d2bd4acc980231
Ardalan Samimi <ardalan.samimi@gmail.com>
2017-03-15 00:21:54 +0100

    Fixed issue where gitprompt wasnt recognized

--- functions/setprompt.zsh
+++ functions/setprompt.zsh
@@ -2,14 +2,14 @@
 
 function setprompt() {
   if [[ -x /usr/bin/git ]]; then
-    GITPROMPT="$(gitprompt branch)"
+    GITPROMPT="$(git_prompt branch)"
 
     if [[ ! -n $GITPROMPT ]]; then
       GITPROMPT="%B%~%b%f%F{white}"
       STSPROMPT=""
     else
       GITPROMPT="%B$(git_remote_name)%b%f%F{white} ${GITPROMPT}"
-      STSPROMPT="$(gitprompt status)"
+      STSPROMPT="$(git_prompt status)"
     fi
   fi
 
