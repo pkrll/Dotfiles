0ceca24133a8f83f55fa71d071975c88f354fe8d
Ardalan Samimi <ardalan.samimi@gmail.com>
2017-09-24 18:49:04 +0200

    Added alias

--- aliases/alias.zsh
+++ aliases/alias.zsh
@@ -1,3 +1,4 @@
 alias makesym="bash ~/.dotfiles/setup/.makesym.sh"
 alias cleanup="bash ~/.dotfiles/setup/.cleanup.sh"
+alias dotfiles="bash ~/.dotfiles/setup/.makesym.sh"
 alias resource=". ~/.zshrc"
