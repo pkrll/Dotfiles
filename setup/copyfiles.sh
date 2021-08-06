ac97c4dc8b90996c30943d9881f775962ec3527e
Ardalan Samimi <ardalan.samimi@gmail.com>
2021-08-07 00:47:13 +0200

    Add copyfiles script

---
+++ setup/copyfiles.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+##########
+# Script for linking dotfiles and other preferences.
+##########
+
+syncdir=~/.dotfiles
+backdir=~/.dotfiles_backup
+
+fileExists() {
+  if [ -f "$1" ] || [ -d "$1" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+linkFilesInFolder() {
+    cd "${syncdir}/${1}"
+    FILES="* .[^.]*"
+    for file in $FILES; do
+        [ -e "$file" ] || continue
+        
+        echo "----------------------------------"
+        echo "Processing $file"
+        
+        if fileExists "${file}"; then
+            backupFile $file $2 $backdir/$1
+        fi
+        
+        echo ""
+        echo "Creating symlink"
+        echo "Source: $syncdir/$1/$file"
+        echo "Target: $2/$file"
+        
+        ln -sf $syncdir/$1/$file $2/$file
+    done
+}
+
+backupFile () {
+    echo ""
+    echo "Moving file..."
+    echo "Source: $2/$1"
+    echo "Target: $3/$1"
+    
+    if fileExists $3/$1; then
+        fileName=$1-`date +%Y-%m-%d-%H%M%S`
+    else
+        fileName=$1
+    fi
+
+    mkdir -p $3
+    mv $2/$1 $3/$fileName
+}
+
+mkdir -p $backdir
+linkFilesInFolder "preferences/home" ~
+linkFilesInFolder "preferences/xcode/CodeSnippets" ~/Library/Developer/Xcode/UserData/CodeSnippets
+linkFilesInFolder "preferences/xcode/FontAndColorThemes" ~/Library/Developer/Xcode/UserData/FontAndColorThemes
+linkFilesInFolder "preferences/xcode/KeyBindings" ~/Library/Developer/Xcode/UserData/KeyBindings
