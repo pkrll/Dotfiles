faf8a1bf14f4175eb40de82548cf4575511c5928
Ardalan Samimi <ardalan.samimi@gmail.com>
2021-08-07 00:47:34 +0200

    Remove emacs and vim settings

--- .emacs.d/elpa/dash-20170810.137/dash-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/dash-20170810.137/dash-autoloads.el"
-;;;;;;  "../../../../.emacs.d/elpa/dash-20170810.137/dash.el") (22961
-;;;;;;  13722 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
