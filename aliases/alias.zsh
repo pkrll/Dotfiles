1fbd3e75a8654ec3d13589d7dce15265417873a2
Ardalan Samimi <ardalan.samimi@gmail.com>
2021-08-07 12:26:05 +0200

    Update aliases

--- aliases/alias.zsh
+++ aliases/alias.zsh
@@ -1,4 +1,4 @@
-alias makesym="bash ~/.dotfiles/setup/.makesym.sh"
-alias cleanup="bash ~/.dotfiles/setup/.cleanup.sh"
-alias dotfiles="bash ~/.dotfiles/setup/.makesym.sh"
+alias symlink="bash ~/.dotfiles/setup/symlink.sh"
+alias install="bash ~/.dotfiles/setup/install.sh"
+alias cleanup="bash ~/.dotfiles/setup/cleanup.sh"
 alias resource=". ~/.zshrc"
